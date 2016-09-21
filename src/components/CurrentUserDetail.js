3345c45837724c85c946c0e28307ba4a668ce7fd
Wataru Kay <wata1991@gmail.com>
2016-09-21 20:51:28 +0900

    Display skill vote count in CurrentUserDetail component

--- src/components/CurrentUserDetail.js
+++ src/components/CurrentUserDetail.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import RaisedButton from 'material-ui/RaisedButton';
 import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
 
 class CurrentUserDetail extends Component {
 
+  calculateSkillsVotes(currentUserData) {
+    const skills2 = currentUserData.skills.toJS()
+    const unique = [...new Set(skills2.map(skill => skill.name))]
+
+    const finalSkills = unique.map(uniqueSkill => {
+      let count = 0;
+      let voters = [];
+      for(let i =0; i<skills2.length; i++) {
+        if(skills2[i].name == uniqueSkill) {
+          count++;
+          let voter = {name:skills2[i].assignee_name, id:skills2[i].assignee_id}
+          voters.push(voter)
+        }
+      }
+      let result = {skillName:uniqueSkill, voteCount:count,voters:voters}
+      return result
+    })
+
+    function compare(a,b) {
+      if (a.voteCount < b.voteCount)
+      //console.log("here")
+        return 1;
+      if (a.voteCount > b.voteCount)
+        return -1;
+      return 0;
+    }
+
+    const ordered = finalSkills.sort(compare);
+    return ordered
+  }
+
   render() {
     const { currentUserData } = this.props
+    const userSkills = this.calculateSkillsVotes(currentUserData)
+    console.log(userSkills)
+
     const styles = {
       chip: {
         margin: 4,
@@ -15,28 +50,43 @@ class CurrentUserDetail extends Component {
         display: 'flex',
         flexWrap: 'wrap',
       },
-    };
+    }
+
     function handleRequestDelete() {
       console.log("chip was pressed")
     }
-    const skills = currentUserData.skills.map((skill)=> {
+
+    const userSkillsWithVotes = userSkills.map((skill) => {
       return(
-        <ul key={skill.id}>
-          <Chip
-            style={styles.chip}
-            onRequestDelete={handleRequestDelete}
-          >
-            {skill.name}
-          </Chip>
-          <li>
-            Skill Name: {skill.name}
-          </li>
-          <li>
-            Skill Assigned By: {skill.assignee_name}
-          </li>
-        </ul>
-      )
+        <Chip
+          style={styles.chip}
+          key={skill.skillName}
+        >
+          <Avatar size={32}>{skill.voteCount}</Avatar>
+          {skill.skillName}
+        </Chip>
+          )
     })
+
+    // const skills = currentUserData.skills.map((skill)=> {
+    //   return(
+    //     <ul key={skill.id}>
+    //       <Chip
+    //         style={styles.chip}
+    //         onRequestDelete={handleRequestDelete}
+    //       >
+    //         {skill.name}
+    //       </Chip>
+    //       <li>
+    //         Skill Name: {skill.name}
+    //       </li>
+    //       <li>
+    //         Skill Assigned By: {skill.assignee_name}
+    //       </li>
+    //     </ul>
+    //   )
+    // })
+
     return(
       <div>
         <ul>
@@ -46,7 +96,7 @@ class CurrentUserDetail extends Component {
           <li>Age: {currentUserData.age}</li>
         </ul>
         Assigned Skills:
-        {skills}
+        {userSkillsWithVotes}
         <RaisedButton
           containerElement={<Link to="/main" />}
           label="back"/>
--- src/components/allUserData.js
+++ src/components/allUserData.js
@@ -9,17 +9,9 @@ class AllUserData extends Component {
       <div>
         <div>All the user data from http://localhost:3000/api/users</div>
         {allUserData.map((user)=> {
-          const skill = user.skills.map((skill)=> {
-            return(
-              <li key={skill.id}>{skill.name}</li>
-            )
-          })
           return(
-          <ul key={user.id}>
-            <Link to={`/user/${user.id}`}>{user.name}</Link>
-            <li>{user.age}</li>
-            <li>{user.job}</li>
-            {skill}
+            <ul key={user.id}>
+              <Link to={`/user/${user.id}`}>{user.name}</Link>
           </ul>
           )
         })}
